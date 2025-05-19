6816c25bb0c24963970692aec0812e996bae3ab1
Angel <endhygranville@gmail.com>
2025-05-19 14:09:31 -0300

    feat: adding configurations and dependencies

---
+++ src/test/java/com/compass/ecommerce_challenge/EcommerceChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.ecommerce_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
