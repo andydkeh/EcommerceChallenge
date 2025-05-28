9337592aef407ae8f823c37fcbbfc014202b4f23
ANGEL LEDEBRUM GRANVILLE <1125231@atitus.local>
2025-05-28 19:58:32 -0300

    feat: add orderItemDTO and finishOrder

--- src/main/java/com/compass/ecommercechallenge/controller/OrderController.java
+++ src/main/java/com/compass/ecommercechallenge/controller/OrderController.java
@@ -1,9 +1,25 @@
 package com.compass.ecommercechallenge.controller;
 
+import com.compass.ecommercechallenge.service.OrderService;
+import org.springframework.http.RequestEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService){
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/finishOrder")
+    public RequestEntity<?> finishOrder(@AuthenticationPrincipal Jwt jwt){
+
+    }
 }
